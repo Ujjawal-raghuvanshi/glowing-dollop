0d5d3ced905694c88c6bef3d54d2ba3c7fb36996
ujjawalraghuvanshi <ujjawalraghuvanshi232@gmail.com>
2023-06-13 19:42:45 +0530

    initial commit

---
+++ b.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define max(a, b) (a < b ? b : a)
+#define min(a, b) (a > b ? b : a)
+#define mod 998244353
+#define infinity 1000000000000000000
+#ifndef ONLINE_JUDGE
+#include "myprettyprint.hpp"
+#else
+#define dbg(...)
+#endif
+
+int32_t main()
+{
+    int q;
+    cin>>q;
+    while(q--){
+         int n;
+         cin>>n;
+    }
+    return 0;
+}
